dfd2403be966c00c78c7180ef9a659f8515e6543
aliferds <aliferduartesilva@gmail.com>
2025-06-05 14:16:36 -0300

    Cleaning the prototype

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <h1>H1</h1>
+    <>
+      <h1>Portifolio</h1>
+      <h2>BIO</h2>
+      <h2>Projetos</h2>
+      <h2>Contatos</h2>
+    </>
   );
 }
 
