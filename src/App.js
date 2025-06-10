7a8c25b4fdff31a7fa8fc469df0f7663a695882c
aliferds <aliferduartesilva@gmail.com>
2025-06-10 18:06:34 -0300

    Path pattern

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+import './Styles/App.css';
 
 function App() {
   return (
     <>
       <h1>Portifolio</h1>
       <h2>BIO</h2>
-      <h2>Projetos</h2>
-      <h2>Contatos</h2>
+      <h2>Projects</h2>
+      <h3>Web sites</h3>
+      <h3>Components</h3>
+      <h2>Contacts</h2>
     </>
   );
 }
